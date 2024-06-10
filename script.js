672a98c19c47717b61d1823136fc8d51cb26c019
Lolasooo <101289723+Lolasooo@users.noreply.github.com>
2024-06-10 13:01:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const sections = document.querySelectorAll("section");
     const footer = document.querySelector("footer");
 
+    // Theme Toggle
     themeToggle.addEventListener("click", function() {
         body.classList.toggle("dark-mode");
         header.classList.toggle("dark-mode");
         sections.forEach(section => section.classList.toggle("dark-mode"));
-        footer.classList
+        footer.classList.toggle("dark-mode");
+    });
+
+    // Background Toggle
+    const backgrounds = ['bg-1', 'bg-2', 'bg-3', 'bg-4'];
