a2fff06c043352daa3b436fd1997cbaa53db7464
Lolasooo <101289723+Lolasooo@users.noreply.github.com>
2024-06-10 18:27:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,23 @@ document.addEventListener("DOMContentLoaded", function() {
         sections.forEach(section => section.classList.toggle("dark-mode"));
         footer.classList.toggle("dark-mode");
     });
+    themeToggle.addEventListener("touch", function() {
+        body.classList.toggle("dark-mode");
+        header.classList.toggle("dark-mode");
+        sections.forEach(section => section.classList.toggle("dark-mode"));
+        footer.classList.toggle("dark-mode");
+    });
 
     // Background Toggle
     const backgrounds = ['bg-1', 'bg-2', 'bg-3', 'bg-4'];
     let currentBackgroundIndex = 0;
 
     backgroundToggle.addEventListener("click", function() {
+        body.classList.remove(backgrounds[currentBackgroundIndex]);
+        currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
+        body.classList.add(backgrounds[currentBackgroundIndex]);
+    });
+    backgroundToggle.addEventListener("touch", function() {
         body.classList.remove(backgrounds[currentBackgroundIndex]);
         currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
         body.classList.add(backgrounds[currentBackgroundIndex]);
